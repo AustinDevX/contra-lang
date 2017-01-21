d43c944f253520d33ae3c31829e37bbcfef43791
Kadeem Austin <kadeemaustin195@gmail.com>
2017-01-21 10:15:49 -0400

    Adds unimplemented string token class

---
+++ src/frontend/StringToken.cc
@@ -0,0 +1,5 @@
+//
+// Created by austindevx on 1/21/17.
+//
+
+#include "StringToken.h"
---
+++ src/frontend/StringToken.h
@@ -0,0 +1,14 @@
+//
+// Created by austindevx on 1/21/17.
+//
+
+#ifndef CONTRA_LANG_STRINGTOKEN_H
+#define CONTRA_LANG_STRINGTOKEN_H
+
+
+class StringToken {
+
+};
+
+
+#endif //CONTRA_LANG_STRINGTOKEN_H
